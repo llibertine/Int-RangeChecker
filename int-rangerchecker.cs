299cd3f3db7dc444c6b531ca840ee655d56882f6
MFEIL <43759812+llibertine@users.noreply.github.com>
2019-01-25 16:08:56 +0100

    Update
    Included the number checking logic into a method and replaced the ifs with performance-friendly else-if.

--- int-rangerchecker.cs
+++ int-rangerchecker.cs
@@ -1,8 +1,38 @@
 using System;
 
 class MainClass {
+
+  static void NumberChecker(int first, int second)
+  {
+    // Check if one of them is in range. If not, display that both are out of range.
+    if ((first < 11 && first > -11) || (second < 11 && second > -11))
+    {
+      // Check if both numbers are in range
+      if ((first < 11 && first > -11) && (second < 11 && second > -11))
+      {
+        Console.WriteLine("Both numbers are in range!");
+      }
+      // First in range, second not in range
+      else if ((first < 11 && first > -11) && !(second < 11 && second > -11))
+      {
+        Console.WriteLine("First number is in range! The second is not.");
+      }
+      // First not in range, second in range
+      else if (!(first < 11 && first > -11) && (second < 11 && second > -11))
+      {
+        Console.WriteLine("Second number is in range! The first one is not.");
+      }
+    }
+    else
+    {
+      // None of the two numbers are in range.
+      Console.WriteLine("Both numbers are not in range!");
+    }
+  }
+
   public static void Main (string[] args) {
     
+    // Write instructions on screen and take first and second number
     Console.WriteLine("-------------------------------------------------------");
     
     Console.WriteLine("Let's check if one or both of your numbers are in range.");
@@ -17,29 +47,7 @@ class MainClass {
 
     int second = Convert.ToInt32(Console.ReadLine());
 
-    // Check if one of them is in range. If not, display that both are out of range.
-    if ((first < 11 && first > -11) || (second < 11 && second > -11))
-    {
-      // Check if both numbers are in range
-      if ((first < 11 && first > -11) && (second < 11 && second > -11))
-      {
-        Console.WriteLine("Both numbers are in range!");
-      }
-      // First in range, second not in range
-      if ((first < 11 && first > -11) && !(second < 11 && second > -11))
-      {
-        Console.WriteLine("First number is in range! The second is not.");
-      }
-      // First not in range, second in range
-      if (!(first < 11 && first > -11) && (second < 11 && second > -11))
-      {
-        Console.WriteLine("Second number is in range! The first one is not.");
-      }
-    }
-    else
-    {
-      // None of the two numbers are in range.
-      Console.WriteLine("Both numbers are not in range!");
-    }
+    // Call NumberChecker-method
+    NumberChecker(first, second);
   }
 }
